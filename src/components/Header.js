d19c48983fd78b2aa16c2a98e14558a25c80de24
anjalii013 <90517471+anjalii013@users.noreply.github.com>
2022-01-10 01:06:48 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <nav
+        className='navbar navbar-light'
+        style={{ backgroundColor: '#e3f2fd' }}
+      >
+        <div className='container-fluid'>
+          <span className='navbar-brand mb-0 h1'>Customers</span>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/redux/actions/user-action.js
@@ -0,0 +1,8 @@
+import { ActionTypes } from '../constants/action-type'
+
+export const setUsers = (users) => {
+  return {
+    type: ActionTypes.SET_USERS,
+    payload: users,
+  }
+}
